45fd4b6ccfe81fec362eeaeec0f6c396f7348cd8
Nishant Dubey <120732319+Nishantdubey11@users.noreply.github.com>
2023-04-20 10:13:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+$(document).ready(function(){
+      $("#submit").click(function(){
+        var title = $("#title").val();
+        var author = $("#author").val();
+        var isbn = $("#isbn").val();
+        var row = "<tr><td>" + title + "</td><td>" + author + "</td><td>" + isbn + "</td><td><button type='button' class='btn btn-danger delete'>X</button></td></tr>";
+        $("#book-list").append(row);
+        $("#title").val("");
+        $("#author").val("");
+        $("#isbn").val("");
+      });
+
+      $(document).on("click", ".delete", function(){
+        $(this).closest("tr").remove();
+      });
+    });
